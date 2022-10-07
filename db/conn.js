4edaf570175a48e8263b9e3de8c6c0ad41d0dbd3
Aylla Duarte <aylladuarte@outlook.com>
2022-10-07 13:29:08 -0300

    Atualizando conexão com o sequelize

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
+const conn = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -10,4 +10,4 @@ const sequelize = new Sequelize(
   }
 );
 
-module.exports = sequelize;
+module.exports = conn;
