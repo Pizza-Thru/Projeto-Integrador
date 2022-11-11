665a75bd5640674f36f1095d56c7317be31ac6c1
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-11-11 00:14:49 -0400

    Feat: Deletado linha de Debito.

--- public/js/scriptSelectPay.js
+++ public/js/scriptSelectPay.js
@@ -1,12 +1,11 @@
 const formulario = document.forms['formadepagamento'];
 formulario.addEventListener('submit', function(event){
-  const idUser= req.session.userid;
   event.preventDefault();
   const formadepagamento = document.querySelector('input[name="Name"]:checked');
   console.log(formadepagamento);
   if (formadepagamento.value === 'cartaocredito') {
     window.location.href = '/pagamentocredito';
   } else if (formadepagamento.value === 'pix') {
-    window.location.href = `/pagamentopix/${idUser}`;
+    window.location.href = '/pagamentopix';
   }
 });
